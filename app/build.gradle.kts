2519dd16c335b85e9c6924dae258532160b4d037
chandanasmenon <chandanamenon2019@gmail.com>
2025-03-10 17:03:10 +0530

    Setup dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -33,15 +35,60 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-
+    // Core Android Libraries
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
+    // UI Libraries
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
+
+    // Networking
+    implementation(libs.retrofit)
+    implementation(libs.retrofit2.converter.gson)
+
+    // Lifecycle and ViewModel
+    implementation(libs.lifecycle.extensions)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // Dependency Injection
+    implementation(libs.dagger)
+    kapt(libs.dagger.compiler)
+
+    // Firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.google.firebase.auth)
+
+    // AndroidX Credentials API
+    implementation(libs.androidx.credentials.v120)
+    implementation(libs.androidx.credentials.play.services.auth.v120)
+
+    // Google Identity Library
+    implementation(libs.googleid)
+
+    // Room Database
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    kapt(libs.androidx.room.compiler)
+
+    // Unit Testing
     testImplementation(libs.junit)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.androidx.core.testing)
+    testImplementation(libs.jetbrains.kotlinx.coroutines.test)
+    testImplementation(libs.turbine)
+
+    // UI Testing
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.espresso.contrib)
+    androidTestImplementation(libs.jetbrains.kotlinx.coroutines.test)
 }
